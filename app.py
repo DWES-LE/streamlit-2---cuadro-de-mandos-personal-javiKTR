56f0b2ceb3049ba4f55084c14a15b65f61c4e64e
javiKTR <javieraznarcau@gmail.com>
2023-03-02 10:32:11 +0100

    Inicio

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import streamlit as st
+
+# Cargamos los datos
+URL = 'top_100_streamed_songs.csv'
+st.title('Datos de las 100 canciones más escuchadas en Spotify')
+
+df = pd.read_csv(URL)
+
+# Mostramos los datos
+st.write(df)
